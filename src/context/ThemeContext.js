a98b41e416f15277b0e1700db761696c35c28e4c
Neryos <brhv0000@gmail.com>
2022-11-16 14:53:23 +0200

    Added dark/light mode switch

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,6 +8,8 @@ const themeReducer = (state, action) => {
     switch (action.type) {
         case 'CHANGED_COLOR':
             return { ...state, color: action.payload}
+        case 'CHANGED_MODE':
+            return {...state, mode: action.payload}
         default:
             return state
     }
@@ -16,15 +18,19 @@ const themeReducer = (state, action) => {
 
 export function ThemeProvider({ children }){
     const [state,dispatch] = useReducer(themeReducer, {
-        color: '#58249c'
+        color: '#58249c',
+        mode: 'dark'
     })
 
     const changeColor = (color) => {
         dispatch({ type: 'CHANGED_COLOR', payload: color})
     }
+    const changeMode = (mode) => {
+        dispatch({type: 'CHANGED_MODE', payload: mode})
+    }
 
     return(
-        <ThemeContext.Provider value={{...state, changeColor}}>
+        <ThemeContext.Provider value={{...state, changeColor, changeMode}}>
             {children}
         </ThemeContext.Provider>
     )
